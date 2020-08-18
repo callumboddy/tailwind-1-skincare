1c1d94df4de1d067d32327b971de69349f3338fe
Callum Boddy <callum.boddy@gmail.com>
2020-08-18 15:30:38 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BsArrowDownShort } from 'react-icons/bs'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="relative bg-yellow-200 h-screen p-4 md:px-12 lg:px-20 max-w-screen-xl mx-auto">
+      <header className='py-8 flex items-center'>
+      <h1 className='flex-1 text-lg font-bold' >Company.</h1>
+      <nav>
+        <ul className='flex items-center'>
+          <li className='text font-light px-5 py-1 cursor-pointer rounded-full hover:font-medium hover:bg-yellow-300'>Shop</li>
+          <li className='text font-light px-5 py-1 cursor-pointer rounded-full hover:font-medium hover:bg-yellow-300'>About</li>
+          <li className='ml-4 flex items-baseline text text-yellow-200 bg-gray-900 cursor-pointer hover:bg-gray-800 px-3 py-1 text-center rounded-full'>2</li>
+        </ul>
+      </nav>
       </header>
+      <main>
+        <HeroModule />
+      </main>
     </div>
   );
+}
+
+function HeroModule() {
+  return (
+  <div className='relative max-h-full overflow-hidden'>
+    <div className='flex justify-end'>
+      <img className='opacity-25 sm:opacity-100 sm:mt-0 max-h-screen md:w-3/4 lg:w-7/12 flex-none object-cover overflow-hidden' src='https://images.unsplash.com/photo-1571781926291-c477ebfd024b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1834&q=80' alt='Skincare products'/>
+    </div>
+    <div className='absolute top-0 mt-16'>
+      <h2 className='text-6xl font-medium leading-none tracking-wider'>
+        Kind on your skin.
+        <br />
+        Kind on the planet.
+      </h2>
+      <p className='mt-6 max-w-md'>
+      Nullam id dolor id nibh ultricies vehicula ut id elit. Nullam id dolor id nibh ultricies vehicula ut id elit.
+      </p>
+      <a href='#' className='mt-4 inline-block px-6 py-2 bg-gray-900 text-yellow-200 font-bold rounded-full text-sm'>Shop now</a>
+    </div>
+    <div className='absolute bottom-0 mb-40 flex items-center'>
+        <p className='text-sm'>Learn more</p>
+        <BsArrowDownShort className='text-yellow-200 bg-gray-900 rounded-full ml-2' size={24} />
+      </div>
+  </div>
+  )
 }
 
 export default App;
