12eb0704283d28aaa03d8633881295e561e81a99
Callum Boddy <callum.boddy@gmail.com>
2020-08-18 16:01:45 +0100

    tweaks

--- src/App.js
+++ src/App.js
@@ -24,10 +24,10 @@ function App() {
 function HeroModule() {
   return (
   <div className='relative h-screen overflow-hidden'>
-    <div className='ml-32 md:ml-0 flex justify-end'>
+    <div className='ml-20 md:ml-0 flex justify-end'>
       <img className='opacity-25 sm:opacity-100 sm:mt-0 max-h-screen md:w-3/4 lg:w-7/12 flex-none object-cover overflow-hidden' src='https://images.unsplash.com/photo-1571781926291-c477ebfd024b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1834&q=80' alt='Skincare products'/>
     </div>
-    <div className='absolute flex flex-col items-start h-full top-0 pt-16'>
+    <div className='absolute flex flex-col items-start h-full top-0 pt-4 md:pt-16'>
       <h2 className='text-5xl sm:text-6xl font-medium leading-tight sm:leading-none  tracking-wider'>
         Kind on your skin.
         <br />
@@ -37,8 +37,8 @@ function HeroModule() {
       Nullam id dolor id nibh ultricies vehicula ut id elit. Nullam id dolor id nibh ultricies vehicula ut id elit.
       </p>
       <a href='/#' className='static mt-4 px-6 py-2 bg-gray-900 text-yellow-200 font-bold rounded-full text-sm'>Shop now</a>
-      <div className='flex-1 mb-40'></div>
-      <div className='flex items-center mb-40'>
+      <div className='flex-1 mb-10'></div>
+      <div className='flex items-center mb-48'>
         <p className='text-sm'>Learn more</p>
         <BsArrowDownShort className='text-yellow-200 bg-gray-900 rounded-full ml-2' size={24} />
       </div>
